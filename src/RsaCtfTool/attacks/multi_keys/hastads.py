07c4d7f3b2010cd5b7086edfbb0ef52f234d4355
vishwaksen-1 <newtonlucky35@gmail.com>
2026-09-10 22:25:13 +0530

    Fix hangs and edge cases in factorization attacks

--- src/RsaCtfTool/attacks/multi_keys/hastads.py
+++ src/RsaCtfTool/attacks/multi_keys/hastads.py
@@ -18,7 +18,7 @@ class Attack(AbstractAttack):
         if not isinstance(publickeys, list):
             return None, None
 
-        if cipher is None or len(cipher) == 0:
+        if cipher is None or len(cipher) != len(publickeys):
             return None, None
 
         ciphers = [int.from_bytes(c, "big") for c in cipher]
